9dbd0f5e6a5792c5787dbdfa29311cea760b5223
zhangzb2007 <zhangzb2007@gmail.com>
2014-12-18 23:17:41 +0800

    Third print

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		System.out.println("The second Hello");
+		System.out.println("The third Hello");
 	}
 
 }
