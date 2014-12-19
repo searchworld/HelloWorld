a8d68e7b5a1cd874b7200ce9e8262ee0e0193f3f
zhangzb2007 <zhangzb2007@gmail.com>
2014-12-19 12:24:16 +0800

    The forth print

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello world");
 		System.out.println("The second Hello");
 		System.out.println("The third Hello");
+		System.out.println("The forth Hello");
 	}
 
 }
